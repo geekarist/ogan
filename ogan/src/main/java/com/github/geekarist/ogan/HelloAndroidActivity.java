a92b2f02e528d0c89ee687c868171c61b140a763
Geekarist <christophe.pele@gmail.com>
2013-03-27 22:28:05 +0100

    Write first unit test for showing hardcoded list of calories by day

--- ogan/src/main/java/com/github/geekarist/ogan/HelloAndroidActivity.java
+++ ogan/src/main/java/com/github/geekarist/ogan/HelloAndroidActivity.java
@@ -1,14 +1,21 @@
 package com.github.geekarist.ogan;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 
 public class HelloAndroidActivity extends Activity {
 
     private static String TAG = "ogan";
 
-    /**
+	public HelloAndroidActivity(ArrayAdapter<String> adapterMock) {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
      * Called when the activity is first created.
      * @param savedInstanceState If the activity is being re-initialized after 
      * previously being shut down then this Bundle contains the data it most 
@@ -20,6 +27,11 @@ public class HelloAndroidActivity extends Activity {
 		Log.i(TAG, "onCreate");
         setContentView(R.layout.main);
     }
+
+	public List<String> getListItems() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
 
