4bce5bcc3ff8e43a840bd365991c90995e929a0f
Geekarist <christophe.pele@gmail.com>
2013-03-30 14:02:00 +0100

    Add ListView to layout and update unit test for loading list items

--- ogan/ogan-it/src/main/java/com/github/geekarist/ogan/test/HelloAndroidActivityTest.java
+++ ogan/ogan-it/src/main/java/com/github/geekarist/ogan/test/HelloAndroidActivityTest.java
@@ -1,6 +1,12 @@
 package com.github.geekarist.ogan.test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import junit.framework.Assert;
+import android.os.Bundle;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.ArrayAdapter;
 
 import com.github.geekarist.ogan.HelloAndroidActivity;
 
@@ -10,26 +16,35 @@ public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<H
         super(HelloAndroidActivity.class); 
     }
 
-    public void testActivity() {
+    public void shouldBeCreatedProperly() {
+    	// GIVEN
+        // WHEN
         HelloAndroidActivity activity = getActivity();
+        // THEN
         assertNotNull(activity);
     }
 
-//	public void shouldLoadWeekItemsOnCreate() {
-//		// GIVEN
-//		@SuppressWarnings("unchecked")
-//		ArrayAdapter<String> adapterMock = Mockito.mock(ArrayAdapter.class);
-//		HelloAndroidActivity activity = new HelloAndroidActivity(adapterMock);
-//		Bundle bundle = new Bundle();
-//		// WHEN
-//		activity.onCreate(bundle);
-//		// THEN
-//		List<String> expectedItems = Arrays.asList("Monday: 0", "Tuesday: 0",
-//				"Wednesday: 0", "Thursday: 0", "Friday: 0", "Saturday: 0",
-//				"Sunday: 0");
-//		List<String> listItems = activity.getListItems();
-//		Assert.assertTrue(listItems.containsAll(expectedItems));
-//		Assert.assertTrue(expectedItems.containsAll(listItems));
-//	}
+	public void shouldLoadWeekItemsOnCreate() {
+		// GIVEN
+		ArrayAdapter<String> adapterMock = mock(ArrayAdapter.class);
+		HelloAndroidActivity activity = new HelloAndroidActivity(adapterMock);
+		Bundle bundle = new Bundle();
+		
+		// WHEN
+		activity.onCreate(bundle);
+		
+		// THEN
+		List<String> expectedItems = Arrays.asList("Monday: 0", "Tuesday: 0",
+				"Wednesday: 0", "Thursday: 0", "Friday: 0", "Saturday: 0",
+				"Sunday: 0");
+		List<String> listItems = activity.getListItems();
+		Assert.assertTrue(listItems.containsAll(expectedItems));
+		Assert.assertTrue(expectedItems.containsAll(listItems));
+	}
+
+	private ArrayAdapter<String> mock(Class<?> class1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
 
--- ogan/ogan/src/main/java/com/github/geekarist/ogan/HelloAndroidActivity.java
+++ ogan/ogan/src/main/java/com/github/geekarist/ogan/HelloAndroidActivity.java
@@ -1,12 +1,19 @@
 package com.github.geekarist.ogan;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import de.akquinet.android.androlog.Log;
 
 public class HelloAndroidActivity extends Activity {
 
-    /**
+    public HelloAndroidActivity(ArrayAdapter<String> adapterMock) {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
      * Called when the activity is first created.
      * @param savedInstanceState If the activity is being re-initialized after
      * previously being shut down then this Bundle contains the data it most
@@ -24,6 +31,11 @@ public class HelloAndroidActivity extends Activity {
 
         setContentView(R.layout.main);
     }
+
+	public List<String> getListItems() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
 
