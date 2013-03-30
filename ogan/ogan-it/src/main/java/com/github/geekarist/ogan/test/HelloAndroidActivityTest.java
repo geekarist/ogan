b7e31df20a895e8ef1dd4504ee21be82de6cc60b
Geekarist <christophe.pele@gmail.com>
2013-03-30 13:42:29 +0100

    Use maven archetype that provides tests

--- ogan/ogan-it/src/main/java/com/github/geekarist/ogan/test/HelloAndroidActivityTest.java
+++ ogan/ogan-it/src/main/java/com/github/geekarist/ogan/test/HelloAndroidActivityTest.java
@@ -1,7 +1,8 @@
 package com.github.geekarist.ogan.test;
 
 import android.test.ActivityInstrumentationTestCase2;
-import com.github.geekarist.ogan.*;
+
+import com.github.geekarist.ogan.HelloAndroidActivity;
 
 public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
 
@@ -13,5 +14,22 @@ public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<H
         HelloAndroidActivity activity = getActivity();
         assertNotNull(activity);
     }
+
+//	public void shouldLoadWeekItemsOnCreate() {
+//		// GIVEN
+//		@SuppressWarnings("unchecked")
+//		ArrayAdapter<String> adapterMock = Mockito.mock(ArrayAdapter.class);
+//		HelloAndroidActivity activity = new HelloAndroidActivity(adapterMock);
+//		Bundle bundle = new Bundle();
+//		// WHEN
+//		activity.onCreate(bundle);
+//		// THEN
+//		List<String> expectedItems = Arrays.asList("Monday: 0", "Tuesday: 0",
+//				"Wednesday: 0", "Thursday: 0", "Friday: 0", "Saturday: 0",
+//				"Sunday: 0");
+//		List<String> listItems = activity.getListItems();
+//		Assert.assertTrue(listItems.containsAll(expectedItems));
+//		Assert.assertTrue(expectedItems.containsAll(listItems));
+//	}
 }
 
