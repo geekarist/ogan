e69444999b56a81480a8b40032917e28896bf5d0
Geekarist <christophe.pele@gmail.com>
2013-03-30 18:56:55 +0100

    Load list of days with hardcoded calories

--- ogan/ogan-it/src/main/java/com/github/geekarist/ogan/test/HelloAndroidActivityTest.java
+++ ogan/ogan-it/src/main/java/com/github/geekarist/ogan/test/HelloAndroidActivityTest.java
@@ -1,50 +1,59 @@
 package com.github.geekarist.ogan.test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import junit.framework.Assert;
 import android.os.Bundle;
 import android.test.ActivityInstrumentationTestCase2;
-import android.widget.ArrayAdapter;
+import android.test.UiThreadTest;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.github.geekarist.ogan.HelloAndroidActivity;
 
-public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
+public class HelloAndroidActivityTest extends
+		ActivityInstrumentationTestCase2<HelloAndroidActivity> {
 
-    public HelloAndroidActivityTest() {
-        super(HelloAndroidActivity.class); 
-    }
+	private HelloAndroidActivity activity;
+	private ListView listView;
 
-    public void shouldBeCreatedProperly() {
-    	// GIVEN
-        // WHEN
-        HelloAndroidActivity activity = getActivity();
-        // THEN
-        assertNotNull(activity);
-    }
+	public HelloAndroidActivityTest() {
+		super(HelloAndroidActivity.class);
+	}
 
+	@Override
+	protected void setUp() throws Exception {
+		activity = getActivity();
+		listView = (ListView) activity
+				.findViewById(com.github.geekarist.ogan.R.id.listView1);
+	}
+
+	@UiThreadTest
 	public void shouldLoadWeekItemsOnCreate() {
 		// GIVEN
-		ArrayAdapter<String> adapterMock = mock(ArrayAdapter.class);
-		HelloAndroidActivity activity = new HelloAndroidActivity(adapterMock);
 		Bundle bundle = new Bundle();
-		
+
 		// WHEN
 		activity.onCreate(bundle);
-		
+
 		// THEN
 		List<String> expectedItems = Arrays.asList("Monday: 0", "Tuesday: 0",
 				"Wednesday: 0", "Thursday: 0", "Friday: 0", "Saturday: 0",
 				"Sunday: 0");
-		List<String> listItems = activity.getListItems();
-		Assert.assertTrue(listItems.containsAll(expectedItems));
-		Assert.assertTrue(expectedItems.containsAll(listItems));
+		assertEquals(7, listView.getChildCount());
+		assertEquals(expectedItems, getChildren(listView));
 	}
 
-	private ArrayAdapter<String> mock(Class<?> class1) {
-		// TODO Auto-generated method stub
-		return null;
+	private List<String> getChildren(ListView listView2) {
+		List<String> result = new ArrayList<String>();
+		for (int i = 0; i < listView2.getChildCount(); i++) {
+			View childAt = listView2.getChildAt(i);
+			final String text = ((TextView) childAt).getText().toString();
+			result.add(text);
+		}
+		return result;
 	}
+
 }
-
--- ogan/ogan/src/main/java/com/github/geekarist/ogan/HelloAndroidActivity.java
+++ ogan/ogan/src/main/java/com/github/geekarist/ogan/HelloAndroidActivity.java
@@ -1,41 +1,47 @@
 package com.github.geekarist.ogan;
 
+import java.util.Arrays;
 import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import de.akquinet.android.androlog.Log;
 
 public class HelloAndroidActivity extends Activity {
 
-    public HelloAndroidActivity(ArrayAdapter<String> adapterMock) {
-		// TODO Auto-generated constructor stub
+	/**
+	 * Called when the activity is first created.
+	 * 
+	 * @param savedInstanceState
+	 *            If the activity is being re-initialized after previously being
+	 *            shut down then this Bundle contains the data it most recently
+	 *            supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it
+	 *            is null.</b>
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Initializes the logging
+		Log.init();
+
+		// Log a message (only on dev platform)
+		Log.i(this, "onCreate");
+
+		setContentView(R.layout.main);
+		ListView listView = (ListView) findViewById(R.id.listView1);
+		List<String> items = loadItems();
+		listView.setAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, android.R.id.text1, items));
 	}
 
-	/**
-     * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after
-     * previously being shut down then this Bundle contains the data it most
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // Initializes the logging
-        Log.init();
-
-        // Log a message (only on dev platform)
-        Log.i(this, "onCreate");
-
-        setContentView(R.layout.main);
-    }
-
-	public List<String> getListItems() {
-		// TODO Auto-generated method stub
-		return null;
+	private List<String> loadItems() {
+		return Arrays
+				.asList("Monday: 0", "Tuesday: 0", "Wednesday: 0",
+						"Thursday: 0", "Friday: 0", "Saturday: 0",
+						"Sunday: 0");
 	}
 
 }
-
